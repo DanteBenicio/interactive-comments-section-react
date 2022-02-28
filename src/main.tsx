e0e9bcbcb5963b5dd1709b9033b8e00e3f60535c
DanteBenicio <dantecosta79@gmail.com>
2022-02-28 14:20:43 -0300

    swapped theme to colors

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 import GlobalStyle from './styles/global-styles';
-import { theme } from './theme';
+import { colors } from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={colors}>
       <App />
     </ThemeProvider>
   </React.StrictMode>,
