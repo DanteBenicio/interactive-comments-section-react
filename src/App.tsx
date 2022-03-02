944a292148c2e1e58ec050ec69b11da8221e2f08
DanteBenicio <dantecosta79@gmail.com>
2022-03-02 20:57:18 -0300

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
-function App() {
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Comment from './components/Comment';
+import Section from './components/Section';
+import { IComment } from './types/comment';
+
+export default function App() {
+  const [comments, setComments] = useState<IComment[] | []>([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/comments?limit=2');
+        const { data } = response;
+
+        setComments(data);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+
+    return () => setComments([]);
+  }, []);
+
   return (
-    <h1>Hello World</h1>
-  )
+    <Section>
+      {comments.map((comment) => (
+        <Comment
+          content={comment.content}
+          createdAt={comment.createdAt}
+          score={comment.score}
+          user={comment.user}
+          replies={comment.replies}
+        />
+      ))}
+    </Section>
+  );
 }
 
 export default App
