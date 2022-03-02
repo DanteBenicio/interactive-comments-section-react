e06710fca88b9b662b536a39a1d45a28db83c9cb
DanteBenicio <dantecosta79@gmail.com>
2022-03-02 20:57:51 -0300

    import theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 import GlobalStyle from './styles/global-styles';
-import { colors } from './theme';
+import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
