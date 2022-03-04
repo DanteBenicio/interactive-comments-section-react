34d7d7897eeb8ca578b9842f6f41b8ff57f266c1
DanteBenicio <dantecosta79@gmail.com>
2022-03-04 09:57:13 -0300

    Section styles

--- src/components/Section/styles.ts
+++ src/components/Section/styles.ts
@@ -5,12 +5,13 @@ export const Container = styled.section`
   min-height: 100vh;
   display: grid;
   place-items: center;
+  padding: 3.2rem 1.6rem;
 `;
 
 export const Wrapper = styled.main`
   display: flex;
   flex-direction: column;
-  gap: 1rem;
+  gap: 1.6rem;
   max-width: 600px;
   padding: 1rem;
 `;
