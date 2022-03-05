78ba33e1af70ef63c07182a8edf788db8f4701e0
DanteBenicio <dantecosta79@gmail.com>
2022-03-05 12:00:12 -0300

    comment replies types

--- src/types/replies.ts
+++ src/types/replies.ts
@@ -1,5 +1,11 @@
 import { IComment } from './comment';
+import { IShowModal } from './modal';
 
 export interface RepliesType extends IComment {
   replyingTo: string
+}
+
+export interface CommentReplyProps extends RepliesType {
+  setShowModal: (modal: IShowModal) => void
+  editComment: (commentId: number, isReply: boolean) => Promise<void>
 }
