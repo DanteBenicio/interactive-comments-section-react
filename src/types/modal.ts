5e545af7fcad3fc22596f1d9a057c08595af13a8
DanteBenicio <dantecosta79@gmail.com>
2022-03-07 22:56:00 -0300

    created ModalProps type

--- src/types/modal.ts
+++ src/types/modal.ts
@@ -1,4 +1,12 @@
 export interface IShowModal {
   showModal: boolean
   commentId: number | null
+  deleteCommentReply?: (commentId: number) => void
+}
+
+export type ModalProps = {
+  setShowModal: (showModal: IShowModal) => void
+  showModal: IShowModal
+  deleteComment: (commentId: number, allComments: IComment[]) => void
+  isCommentReply: boolean
 }
