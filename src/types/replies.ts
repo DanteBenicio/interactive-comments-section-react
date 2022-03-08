58e984a0687d5f9eb6b2bd0ebdd1210b9b03dd10
DanteBenicio <dantecosta79@gmail.com>
2022-03-07 22:58:50 -0300

    import CurrentUserType type

--- src/types/replies.ts
+++ src/types/replies.ts
@@ -1,5 +1,6 @@
 import { Dispatch, SetStateAction } from 'react';
 import { IComment } from './comment';
+import { CurrentUserType } from './currentUser';
 import { IShowModal } from './modal';
 
 export interface RepliesType extends IComment {
