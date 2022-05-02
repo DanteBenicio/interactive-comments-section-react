b1c8f96b9d9e2064d07ecec5d77820b3718bd0ea
Dante Benicio <dantecosta79@gmail.com>
2022-05-02 14:36:55 -0300

    import and render Switch component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { ThemeContext } from 'styled-components';
 import { NavbarProps } from '../../types/navbar';
+import Switch from '../Switch';
 import {
   Container, Logo, LogoWrapper, Wrapper,
 } from './styles';
@@ -15,13 +16,8 @@ export default function Navbar({ toggleTheme }: NavbarProps) {
           <Logo src="assets/favicon.png" alt="two comment ballons" />
         </LogoWrapper>
         <Switch
-          onChange={toggleTheme}
-          checked={title !== 'light'}
-          checkedIcon={false}
-          uncheckedIcon={false}
-          handleDiameter={20}
-          offColor="#e2e1e1"
-          onColor="#5457b6"
+          toggleTheme={toggleTheme}
+          checked={title === 'dark'}
         />
       </Wrapper>
     </Container>
