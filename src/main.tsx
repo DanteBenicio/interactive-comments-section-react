ad16f76f8a132b38ec4fef149a634f392f642b5d
Dante Benicio <dantecosta79@gmail.com>
2022-05-01 22:48:11 -0300

    removed ThemeProvider and GlobalStyle

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider } from 'styled-components';
 import App from './App';
 import AppProvider from './context';
-import GlobalStyle from './styles/global-styles';
-import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </ThemeProvider>
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
